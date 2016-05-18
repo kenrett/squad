67e369889f83ee6c92d388098def221a9fe44598
Ken Rettberg <=>
2016-05-17 22:52:40 -0600

    trying a space

--- config/initializers/squadapp.rb
+++ config/initializers/squadapp.rb
@@ -1,4 +1,4 @@
-SquadApp.secret_key_base    = ENV["squadapp_secret_key_base"]
-SquadApp.uber_client_id     = ENV["squadapp_uber_client_id"]
-SquadApp.uber_client_secret = ENV["squadapp_uber_client_secret"]
-SquadApp.uber_server_token  = ENV["squadapp_uber_server_token"]
+SquadApp.secret_key_base    = ENV["squad_app_secret_key_base"]
+SquadApp.uber_client_id     = ENV["squad_app_uber_client_id"]
+SquadApp.uber_client_secret = ENV["squad_app_uber_client_secret"]
+SquadApp.uber_server_token  = ENV["squad_app_uber_server_token"]
