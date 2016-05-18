9f9512948fb868c727ed576f709efa6c6c3a872a
Ken Rettberg <=>
2016-05-17 20:44:36 -0600

    Renamed app SquadApp

--- config/initializers/squad.rb
+++
@@ -1,6 +0,0 @@
-Squad.secret_key_base = ENV["secret_key_base"]
-Squad.uber_client_id = ENV["uber_client_id"]
-Squad.uber_client_secret = ENV["uber_client_secret"]
-Squad.uber_server_token = ENV["uber_server_token"]
-
-Squad.require_keys("secret_key_base","uber_client_id","uber_client_secret","uber_server_token")
