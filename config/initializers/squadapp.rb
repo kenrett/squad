9ed7d5c6115f571120ddbdc9192c04b5d0fee397
Ken Rettberg <=>
2016-05-17 22:46:50 -0600

    figaro take 2

---
+++ config/initializers/squadapp.rb
@@ -0,0 +1,4 @@
+SquadApp.secret_key_base    = ENV["squadapp_secret_key_base"]
+SquadApp.uber_client_id     = ENV["squadapp_uber_client_id"]
+SquadApp.uber_client_secret = ENV["squadapp_uber_client_secret"]
+SquadApp.uber_server_token  = ENV["squadapp_uber_server_token"]
