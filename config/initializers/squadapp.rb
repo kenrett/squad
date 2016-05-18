5523f367df9a910a61e049d1ebc11cee86d26104
Ken Rettberg <=>
2016-05-17 22:59:14 -0600

    commented out the initializer

--- config/initializers/squadapp.rb
+++ config/initializers/squadapp.rb
@@ -1,4 +1,4 @@
-SquadApp.secret_key_base    = ENV["secret_key_base"]
-SquadApp.uber_client_id     = ENV["uber_client_id"]
-SquadApp.uber_client_secret = ENV["uber_client_secret"]
-SquadApp.uber_server_token  = ENV["uber_server_token"]
+# SquadApp.secret_key_base    = ENV["secret_key_base"]
+# SquadApp.uber_client_id     = ENV["uber_client_id"]
+# SquadApp.uber_client_secret = ENV["uber_client_secret"]
+# SquadApp.uber_server_token  = ENV["uber_server_token"]
